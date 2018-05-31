4584469a47e95daade1364083b9f8a7d3f8d924d
Hillevi Eklöw <hillevieklow@gmail.com>
2018-05-31 17:54:01 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,10 @@
 # Write your code here.
+katz_deli = []
 
 def take_a_number(katz_deli, name)
+  katz_deli << name
+  "Welcome, #{name}. You are number #{katz_deli.size + 1} in line."
 end
 
 def now_serving(next_person)
+end
