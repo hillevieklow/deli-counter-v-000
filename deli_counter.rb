1db27bd0b52dff20d9d52a77ccccd1c969a54639
Hillevi Eklöw <hillevieklow@gmail.com>
2018-05-31 18:41:50 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,6 +16,15 @@ def now_serving(katz_deli)
 end
 
 def line(katz_deli)
+  line_position = []
   if katz_deli.empty?
+    puts "The line is currently empty."
+  else
+    number = 1
+    katz_deli.each do |customer|
+      line_position << "#{number}. #{customer}"
+      number += 1
+    end
+    puts "The line is currently: #{line_position.join(" ")}"
   end
 end
