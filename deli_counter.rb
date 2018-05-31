7e6da885c12051d0eadb69f246e05ebe20fba1ac
Hillevi Eklöw <hillevieklow@gmail.com>
2018-05-31 18:31:48 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,14 +3,18 @@ katz_deli = []
 
 def take_a_number(katz_deli, name)
   katz_deli << name
-  "Welcome, #{name}. You are number #{katz_deli.size + 1} in line."
+  puts "Welcome, #{name}. You are number #{katz_deli.size + 1} in line."
 end
 
-def now_serving(line)
-  if line.size == 0
+def now_serving(katz_deli)
+  if katz_deli.size == 0
     puts "There is nobody waiting to be served!"
-  elsif line.size > 0
-    puts "Currently serving #{line[0]}"
-    line.shift
+  elsif katz_deli.size > 0
+    puts "Currently serving #{katz_deli[0]}"
+    katz_deli.shift
   end
+end
+
+def line(katz_deli)
+
 end
